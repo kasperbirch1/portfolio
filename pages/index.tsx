d5859c62f3506872c041db24c57276baf9166e43
Kasper Birch <kasperbirch1@gmail.com>
2022-09-21 19:36:46 +0200

    Test 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,50 +25,49 @@ type Props = {
 const Home = ({ projects, skills, pageInfo, experiences, socials }: Props) => {
   return (
     <div className="snap-y snap-mandatory h-screen overflow-y-scroll scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 bg-[rgb(36,36,36)] text-white z-0">
-      {/* <Header socials={socials} /> */}
-      <h1>Kasoer</h1>
+      <Header socials={socials} />
 
       {/* Hero Banner Section */}
-      {/* <section id="hero" className="snap-start">
-          <Hero pageInfo={pageInfo} />
-        </section> */}
+      <section id="hero" className="snap-start">
+        <Hero pageInfo={pageInfo} />
+      </section>
 
       {/* About Section */}
-      {/* <section id="about" className="snap-center">
-          <About pageInfo={pageInfo} />
-        </section> */}
+      <section id="about" className="snap-center">
+        <About pageInfo={pageInfo} />
+      </section>
 
       {/* Experience Section */}
-      {/* <section id="experience" className="snap-center">
-          <WorkExperience experiences={experiences} />
-        </section> */}
+      <section id="experience" className="snap-center">
+        <WorkExperience experiences={experiences} />
+      </section>
 
       {/* Skills Section */}
-      {/* <section id="skills" className="snap-start">
-          <Skills skills={skills} />
-        </section> */}
+      <section id="skills" className="snap-start">
+        <Skills skills={skills} />
+      </section>
 
       {/* Projects Section */}
-      {/* <section id="projects" className="snap-start">
-          <Projects projects={projects} />
-        </section> */}
+      <section id="projects" className="snap-start">
+        <Projects projects={projects} />
+      </section>
 
       {/* Contact Me Section */}
-      {/* <section id="contact" className="snap-start">
-          <ContactMe pageInfo={pageInfo} />
-        </section> */}
+      <section id="contact" className="snap-start">
+        <ContactMe pageInfo={pageInfo} />
+      </section>
 
-      {/* <Link href="#hero">
-          <footer className="sticky bottom-5 w-full cursor-pointer">
-            <div className="flex items-center justify-center">
-              <img
-                className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
-                src="https://i.imgur.com/e2yvD6A.png"
-                alt=""
-              />
-            </div>
-          </footer>
-        </Link> */}
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <img
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+              src="https://i.imgur.com/e2yvD6A.png"
+              alt=""
+            />
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 };
